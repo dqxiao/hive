d88c025fcb8309ff08ac0c339458ba0a86be3482
dxiao <dqxiaowpi@gmail.com>
2016-05-01 11:00:36 -0400

    package fix

---
+++ compile_fix.sh
@@ -0,0 +1,5 @@
+# copy connector to lib 
+
+cp /usr/local/Cellar/hive/1.2.1/libexec/lib/mysql-connector-java-5.1.38-bin.jar $HIVE_HOME/lib/
+# copy right configuration to conf 
+cp /usr/local/Cellar/hive/1.2.1/libexec/conf/hive-site.xml $HIVE_HOME/conf/
