1629a73fe18fa76289224fe12adcecc074264e11
dxiao <dqxiaowpi@gmail.com>
2016-06-13 22:20:27 -0400

    compile setting

--- compile_fix.sh
+++ compile_fix.sh
@@ -1,5 +1,6 @@
-# copy connector to lib 
+mvn clean package -Pdist -DskipTests
 
+# copy connector to lib 
 cp /usr/local/Cellar/hive/1.2.1/libexec/lib/mysql-connector-java-5.1.38-bin.jar $HIVE_HOME/lib/
 # copy right configuration to conf 
 cp /usr/local/Cellar/hive/1.2.1/libexec/conf/hive-site.xml $HIVE_HOME/conf/
